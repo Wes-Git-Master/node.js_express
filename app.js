3cb81cf5705e618a1dd667894d0a240c2505cd00
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-07-11 00:03:20 +0300

    hw/3---express

--- app.js
+++ app.js
@@ -57,12 +57,87 @@ app.post('/users', (req, res) => {
     users.push(userWithId)
 
     fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users, null, 2), (err) => {
+
         if (err) {
             console.error(err)
             return res.status(500).json({message: 'Internal Server Error'})
         }
 
         res.status(201).json({message: 'User added successfully', user: userWithId})
+    })
+});
+
+//===========================================================================================================
+
+
+app.put('/users/:id', (req, res) => {
+    const userId = parseInt(req.params.id);
+    const updatedUser = req.body;
+    const userIndex = users.findIndex(user => user.id === userId);
+
+    if (userIndex === -1) {
+        return res.status(404).json({message: 'User not found'})
+    }
+
+    const receivedKeys = Object.keys(updatedUser)
+    const missingKeys = allowedKeys.filter(key => !receivedKeys.includes(key))
+
+    if (missingKeys.length > 0) {
+        return res.status(400).json({message: `Missing keys: ${missingKeys.join(', ')}`})
+    }
+
+    const isValidKeys = receivedKeys.every(key => allowedKeys.includes(key))
+
+    if (!isValidKeys) {
+        return res.status(400).json({message: 'Invalid keys provided'})
+    }
+
+    if (updatedUser.name && (typeof updatedUser.name !== 'string' || updatedUser.name.trim().length === 0)) {
+        return res.status(400).json({message: 'Invalid name format'})
+    }
+
+    if (updatedUser.age && (isNaN(updatedUser.age) || typeof updatedUser.age !== 'number' || updatedUser.age < 0)) {
+        return res.status(400).json({message: 'Invalid age format'})
+    }
+
+    if (updatedUser.status && typeof updatedUser.status !== 'boolean') {
+        return res.status(400).json({message: 'Invalid status format'})
+    }
+
+    users[userIndex] = {...users[userIndex], ...updatedUser}
+
+    fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users, null, 2), (err) => {
+
+        if (err) {
+            console.error(err)
+            return res.status(500).json({message: 'Internal Server Error'})
+        }
+
+        res.json({message: 'User updated successfully', user: users[userIndex]})
+    })
+});
+
+//===========================================================================================================
+
+app.delete('/users/:id', (req, res) => {
+
+    const userId = parseInt(req.params.id);
+    const userIndex = users.findIndex(user => user.id === userId);
+    const deletedUser = users.splice(userIndex, 1)[0]
+
+    if (userIndex === -1) {
+        return res.status(404).json({message: 'User not found'})
+    }
+
+    fs.writeFile(path.join(__dirname, 'users.json'), JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+            console.error(err)
+
+            users.splice(userIndex, 0, deletedUser)
+            return res.status(500).json({message: 'Internal Server Error'})
+        }
+
+        res.json({message: 'User deleted successfully', user: deletedUser})
     })
 });
 
