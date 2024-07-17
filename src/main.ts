2aeb8752de188725d133517662562ba91f55fd63
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2024-07-17 18:17:53 +0300

    hw/4

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,8 @@ process.on("uncaughtException", (e) => {
   process.exit(1);
 });
 
+//===========================================================================================================
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
